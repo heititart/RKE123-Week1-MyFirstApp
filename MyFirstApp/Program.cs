ff54eb5088d12754e617a6d1922399ca10ceecfb
Tart Heiti <Heiti.Tart@itella.com>
2023-01-30 19:40:00 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// This is a comment to my code
+
+
+//Rakendus küsib kasutajalt nime
+// Seejärel tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");// output
+// String - sõne
+string userName = Console.ReadLine();//input
+
+Console.WriteLine("Hello" +", "+ userName + "!");//output
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}! ");//input
+
+Console.WriteLine("Sisesta perekonna nimi:");
+string familyName = Console.ReadLine();
+Console.WriteLine($" Tere {userName} {familyName}");
+
